d1c901b078a105bb5ec035ea8fcf645163080795
OlekVano <olekvano@gmail.com>
2023-03-17 15:14:57 +0000

    Added font weights and sizes to Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,18 @@ module.exports = {
       'bg-complementary': '#ebb2b1',
       'text-primary': '#ffffff'
     },
+    fontSize: {
+      'body-small': '20px',
+      'subheading': '25px',
+      'body': '32px',
+      'heading': '35px',
+      'huge': '60px'
+    },
+    fontWeight: {
+      'md': '500',
+      'lg': '700',
+      'xl': '900'
+    },
     extend: {},
   },
   plugins: [],
