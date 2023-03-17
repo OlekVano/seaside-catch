8dec050a84d2125107c445eb9ed3a9d1cd532105
OlekVano <olekvano@gmail.com>
2023-03-17 10:13:01 +0000

    Added colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'bg-primary': '#360CCD',
+      'bg-secondary': '#402DDF',
+      'bg-tertiary': '#2A4EE5',
+      'bg-quaternary': '#1E90FC',
+      'bg-quinary': '#58CCED',
+      'bg-complementary': '#ebb2b1'
+    },
     extend: {},
   },
   plugins: [],
