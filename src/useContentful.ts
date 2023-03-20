324a5a542cd81f8d7b4a5cb661de837ba3ecd1db
OlekVano <olekvano@gmail.com>
2023-03-20 09:23:01 +0000

    Removed host from create client

--- src/useContentful.ts
+++ src/useContentful.ts
@@ -5,7 +5,6 @@ export default function useContentful() {
   const client = createClient({
     space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
     accessToken: import.meta.env.VITE_CONTENTFUL_TOKEN,
-    host: 'preview.contentful.com'
   })
 
   async function getContent() {
