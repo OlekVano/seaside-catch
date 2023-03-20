bd50b52cee01ff151b82d52e4579f5371b6017a2
OlekVano <olekvano@gmail.com>
2023-03-20 13:54:24 +0000

    Landing section medium screen responsiveness

--- src/components/LandingSection/index.tsx
+++ src/components/LandingSection/index.tsx
@@ -8,21 +8,23 @@ export default function LandingSection() {
   const { content } = useContext(ContentContext)
 
   return (
-    <div className='relative min-h-screen'>
+    <div className='relative min-h-screen flex flex-col h-fit'>
       <Birds />
-      <div className='h-full w-full absolute grid place-items-center bg-gradient-to-b from-bg-quaternary to-bg-complementary'>
+      <div className='h-full w-full grid place-items-center bg-gradient-to-b from-bg-quaternary to-bg-complementary flex-1'>
         <div className='h-full z-10 max-w-7xl'>
           <div className='h-full w-full flex p-14'>
-            <div className='w-1/2 flex flex-col justify-center gap-10'>
-              <div className='text-huge uppercase font-black drop-shadow-md'>
+            <div className='flex-1 flex flex-col justify-center gap-10'>
+              <div className='text-huge uppercase font-black drop-shadow-md max-lg:text-center'>
                 {content!.homepage.landingSectionTitle}
               </div>
-              <div className='text-body font-semibold drop-shadow-md'>
+              <div className='text-body font-semibold drop-shadow-md max-lg:text-center'>
                 {content!.homepage.landingSectionText}
               </div>
-              <Button text='Book A Table' />
+              <div className='w-fit mx-auto'>
+                <Button text='Book A Table' />
+              </div>
             </div>
-            <div className='w-1/2 grid place-items-center'>
+            <div className='w-1/2 grid place-items-center max-lg:hidden'>
               <div className='group w-full aspect-square relative grid place-items-center cursor-pointer transition hover:scale-110'>
                 <div className='absolute w-full aspect-square grid place-items-center'>
                   <div className='bg-bg-primary rounded-full w-3/4 aspect-square'></div>
