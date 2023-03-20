2041f18acf2ea3420aff43e0214adaa688cbba3a
OlekVano <olekvano@gmail.com>
2023-03-20 10:04:48 +0000

    Fixed a problem with choose us section cards

--- src/components/ChooseUsSection/index.tsx
+++ src/components/ChooseUsSection/index.tsx
@@ -15,21 +15,21 @@ export default function ChooseUsSection() {
         </div>
         <div className='flex-1 flex gap-10'>
           <div className='flex-1 flex flex-col gap-10'>
-            <div className='flex flex-col items-center justify-around flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
+            <div className='flex flex-col items-center p-5 justify-between flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
               <img className='w-1/2 brightness-0 invert' src={content!.homepage.chooseUsSectionCard1Image} />
               <div className='text-subheading text-center font-bold drop-shadow-md'>{content!.homepage.chooseUsSectionCard1Title}</div>
             </div>
-            <div className='flex flex-col items-center justify-around flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
+            <div className='flex flex-col items-center p-5 justify-between flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
               <img className='w-1/2 brightness-0 invert' src={content!.homepage.chooseUsSectionCard2Image} />
               <div className='text-subheading text-center font-bold drop-shadow-md'>{content!.homepage.chooseUsSectionCard2Title}</div>
             </div>
           </div>
           <div className='flex-1 flex flex-col gap-10'>
-            <div className='flex flex-col items-center justify-around flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
+            <div className='flex flex-col items-center p-5 justify-between flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
               <img className='w-1/2 brightness-0 invert' src={content!.homepage.chooseUsSectionCard3Image} />
               <div className='text-subheading text-center font-bold drop-shadow-md'>{content!.homepage.chooseUsSectionCard3Title}</div>
             </div>
-            <div className='flex flex-col items-center justify-around flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
+            <div className='flex flex-col items-center p-5 justify-between flex-1 aspect-square stained-glass rounded-3xl cursor-pointer transition hover:scale-110'>
               <img className='w-1/2 brightness-0 invert' src={content!.homepage.chooseUsSectionCard4Image} />
               <div className='text-subheading text-center font-bold drop-shadow-md'>{content!.homepage.chooseUsSectionCard4Title}</div>
             </div>
