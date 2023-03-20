8484543ae5b732197e39c27268203a9b02ad2e75
OlekVano <olekvano@gmail.com>
2023-03-20 10:23:02 +0000

    Footer mobile responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,15 +10,15 @@ export default function Footer() {
   const { content } = useContext(ContentContext)
 
   return (
-    <div className='bg-bg-secondary w-full grid place-items-center py-20 relative z-10'>
-      <div className='max-w-7xl flex justify-between'>
-        <div className=' max-w-[33.333%]'>
+    <div className='bg-bg-secondary w-full grid place-items-center py-20 px-10 relative z-10'>
+      <div className='max-w-7xl flex justify-between max-md:flex-col max-md:items-center max-md:gap-20'>
+        <div className=' max-w-[33.333%] max-md:max-w-full'>
           <Logo />
           <div className='drop-shadow-md text-small font-semibold'>
             {content!.footer.text}
           </div>
         </div>
-        <div className='flex flex-col items-center gap-2 max-w-[33.333%]'>
+        <div className='flex flex-col items-center gap-2 max-w-[33.333%] max-md:max-w-full'>
           <div className='text-subheading font-bold pb-4 drop-shadow-md'>Contact Us</div>
           <div className='drop-shadow-md text-small font-semibold'>
             {content!.footer.phoneNumber}
@@ -27,7 +27,7 @@ export default function Footer() {
             {content!.footer.emailAddress}
           </div>
         </div>
-        <div className='flex flex-col items-center gap-2 max-w-[33.333%]'>
+        <div className='flex flex-col items-center gap-2 max-w-[33.333%] max-md:max-w-full'>
           <div className='text-subheading font-bold pb-4 drop-shadow-md'>Links</div>
           <a href={content!.footer.facebookLink} className='flex items-center w-full'>
             <img className='w-9 h-9 p-2 brightness-0 invert' src={facebook} />
