5755cdedd309ab59aad254a36762d173fbe79a8c
OlekVano <olekvano@gmail.com>
2023-03-22 18:50:27 +0000

    Logo mobile responsiveness

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,10 +6,10 @@ export default function Logo() {
   
   return (
     <div className='flex items-center gap-5'>
-      <img className='w-24 h-24 max-xl:h-20 max-xl:w-20' src={content!.footer.logo} />
+      <img className='w-24 h-24 max-xl:h-20 max-xl:w-20 max-md:w-16 max-md:h-16' src={content!.footer.logo} />
       <div>
-        <div className='subheading-text leading-none m-0'>Seaside</div>
-        <div className='heading-text te leading-none m-0 normal-case'>Catch</div>
+        <div className='subheading-text text-center max-md:text-[17.5px] leading-none m-0'>Seaside</div>
+        <div className='heading-text text-center max-md:text-[21px] leading-none m-0 normal-case'>Catch</div>
       </div>
     </div>
   )
