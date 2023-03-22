e03f7216d945deeb69755bcce1cc3acad1d25bb1
OlekVano <olekvano@gmail.com>
2023-03-22 18:36:53 +0000

    Header improvement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Button from '../Button'
 import Logo from '../Logo'
 
@@ -7,14 +7,31 @@ interface Props {
 }
 
 export default function Header({ openModal }: Props) {
+  const [scrollPos, setScrollPos] = useState<number>()
+
+  useEffect(function onComponentMount() {
+    document.addEventListener('scroll', handleScrolling)
+
+    return function removeEventListener() {
+      document.removeEventListener('scroll', handleScrolling)
+    }
+  }, [])
+
   return (
-    <div className='fixed w-full border-b-2 border-b-text-primary bg-bg-secondary z-20'>
-      <div className='max-w-7xl mx-auto flex justify-between items-center p-3'>
+    <div className='fixed w-full z-20'>
+      <div className='max-w-7xl mx-auto flex justify-between items-center py-3 px-10 max-md:py-2 max-md:px-5'>
         <Logo />
         <div className='max-xs:hidden'>
           <Button text='Reservation' onClick={openModal} />
         </div>
       </div>
+      <div className={`absolute top-0 -z-10 w-full h-full bg-text-secondary transition duration-500 ${scrollPos && scrollPos > 200 ? 'translate-y-0' : '-translate-y-full'}`}></div>
     </div>
   )
+
+  // ********************************
+
+  function handleScrolling(e: Event) {
+    setScrollPos(window.scrollY)
+  }
 }
